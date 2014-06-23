e9d37acee7ff7952000745f9c6558aa01af88b37
Riza Fahmi <rizafahmi@gmail.com>
2014-06-23 22:07:06 +0700

    Iterate through item data

--- todone.js
+++ todone.js
@@ -1,6 +1,11 @@
 Todones = new Meteor.Collection('todones');
 
 if (Meteor.isClient) {
+  Template.TodonePanel.helpers({
+    'items': function(){
+      return Todones.find();
+    }
+  });
 }
 
 if (Meteor.isServer) {
